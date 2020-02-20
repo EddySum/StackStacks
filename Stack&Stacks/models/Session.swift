e40f6239126fef8c2258df45ba1fc6e7e7f874b7
Eddy Sumra <adnansumra115@gmail.com>
2020-02-20 14:05:28 -0500

    Created Session Model. Added success/fail test. tested cashout()

---
+++ Stack&Stacks/models/Session.swift
@@ -0,0 +1,15 @@
+//
+//  Sessions.swift
+//  Stack&Stacks
+//
+//  Created by Adnan Sumra on 2/20/20.
+//  Copyright © 2020 Adnan Sumra. All rights reserved.
+//
+
+class Sessions {
+    var type: String
+    
+    init(type: String, price: Double) {
+        self.type = type
+    }
+}
