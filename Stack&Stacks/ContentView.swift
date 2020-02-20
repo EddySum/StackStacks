72bea645ab49dbd8a2e7712f815931b8846a7b55
ms2579 <adnansumra115@gmail.com>
2020-02-20 11:15:13 -0500

    Initial Commit

---
+++ Stack&Stacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stack&Stacks
+//
+//  Created by Adnan Sumra on 2/20/20.
+//  Copyright © 2020 Adnan Sumra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stack&StacksTests/Stack_StacksTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stack_StacksTests.swift
+//  Stack&StacksTests
+//
+//  Created by Adnan Sumra on 2/20/20.
+//  Copyright © 2020 Adnan Sumra. All rights reserved.
+//
+
+import XCTest
+@testable import Stack_Stacks
+
+class Stack_StacksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
