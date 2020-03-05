93e44dd48f46e0b1e329350edb35c6ede7f7df17
Eddy Sumra <adnansumra115@gmail.com>
2020-03-05 16:09:40 -0500

    Added basic session view

---
+++ Stack&Stacks/Sessions.swift
@@ -0,0 +1,50 @@
+//
+//  Sessions.swift
+//  Stack&Stacks
+//
+//  Created by Adnan Sumra on 3/5/20.
+//  Copyright © 2020 Adnan Sumra. All rights reserved.
+//
+
+import SwiftUI
+
+struct Sessions: View {
+    var sessions: [Session];
+    var body: some View {
+        
+        
+        VStack {
+            Rectangle()
+                .fill(Color.green)
+                .frame(height: 125)
+                .overlay(
+                    Text("$255.55")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    )
+            Rectangle()
+                .fill(Color.white)
+                .cornerRadius(25)
+                .offset(y: -25)
+                .overlay(
+                    VStack {
+                    
+                        SessionItem(session: session)
+                       
+                        
+                       Spacer()
+                    }
+                )
+            
+            
+            Spacer()
+            
+        }
+    }
+}
+
+struct Sessions_Previews: PreviewProvider {
+    static var previews: some View {
+        Sessions(sessions: [session, session])
+    }
+}
