37288a60940c9dd7f530d0698faf4e303d2e6db2
Adnan Sumra <adnansumra115@gmail.com>
2020-04-15 15:00:29 -0400

    default tab drawer

--- Stack&Stacks/Views/ContentView.swift
+++ Stack&Stacks/Views/ContentView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject private var sessionService: SessionService
+     @State private var selectedTab = 1
     
     init() {
         self.sessionService = SessionService.init()
     }
     
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             NavigationView {
                 AddSession()
                 .navigationBarTitle(Text("Stats"), displayMode: .inline)
@@ -25,6 +26,7 @@ struct ContentView: View {
                 Image(systemName: "1.square.fill")
                 Text("Stats")
             }
+            .tag(0)
             NavigationView {
                Sessions(sessions: sessionService.sessions)
                 .navigationBarTitle(Text("Home"), displayMode: .inline)
@@ -33,6 +35,7 @@ struct ContentView: View {
                 Image(systemName: "tv.fill")
                 Text("Home")
             }
+            .tag(1)
             NavigationView {
                 AddSession()
                 .navigationBarTitle(Text("Profile"), displayMode: .inline)
@@ -41,6 +44,7 @@ struct ContentView: View {
                 Image(systemName: "3.square.fill")
                 Text("Profile")
             }
+            .tag(2)
         }
         
     }
