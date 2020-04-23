e6fde117e3dcc6e3917e575dd6a497b3d2264c6d
Adnan Sumra <adnansumra115@gmail.com>
2020-04-23 01:30:55 -0400

    barebone session detail view

--- Stack&Stacks/Views/SessionDetail/SessionDetail.swift
+++ Stack&Stacks/Views/SessionDetail/SessionDetail.swift
@@ -11,8 +11,52 @@ import SwiftUI
 struct SessionDetail: View {
     
     var body: some View {
-        VStack {
-            Text("Session Detail")
+        VStack(spacing: 30) {
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(".25/.25 NL Hold’Em")
+                    Text("My House")
+                        .foregroundColor(Color.gray)
+                }.padding(EdgeInsets(top: 25, leading: 25, bottom: 0, trailing: 0))
+                Spacer()
+            }
+            
+            HStack {
+                VStack(spacing: 10) {
+                    Text("Net Profit")
+                    HStack {
+                        Text("$90.0")
+                            .bold()
+                        Image(systemName: "chevron.up")
+                    }
+                }
+            }
+            
+            HStack {
+                Spacer()
+                
+                VStack(spacing: 10) {
+                    Text("Buy In")
+                    Text("$90.0")
+                        .bold()
+                }
+                Spacer()
+                
+                VStack(spacing: 10) {
+                    Text("Peak")
+                    Text("$90.0")
+                        .bold()
+                }
+                Spacer()
+                
+                VStack(spacing: 10) {
+                    Text("Players")
+                    Text("5")
+                        .bold()
+                }
+                Spacer()
+            }
+            Spacer()
         }
     }
 }
