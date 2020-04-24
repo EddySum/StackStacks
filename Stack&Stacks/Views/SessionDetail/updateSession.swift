ffedb13c8c74049be8e48d21653e1b97784c2bda
Adnan Sumra <adnansumra115@gmail.com>
2020-04-24 00:10:13 -0400

    update session view

--- Stack&Stacks/Views/SessionDetail/updateSession.swift
+++ Stack&Stacks/Views/SessionDetail/updateSession.swift
@@ -9,8 +9,25 @@
 import SwiftUI
 
 struct updateSession: View {
+    let screenSize: CGRect = UIScreen.main.bounds
+    
+    @State var cashout: String = "5.00";
+    
     var body: some View {
-        Text("Update Session")
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Current Stack: $")
+                    .bold()
+                TextField("", text: $cashout)
+                    .keyboardType(.decimalPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 100)
+                    .font(Font.system(size: 25))
+                       
+            }
+            .padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 0))
+            
+        }.navigationBarTitle(Text("Update Stack"))
     }
 }
 
